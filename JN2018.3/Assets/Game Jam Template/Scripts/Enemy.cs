adb369e67eff886ca079440807f81de1af872e8b
fecfec <leandre_w_bolduc@hotmail.fr>
2019-01-25 23:46:42 -0500

    Added basic slap

---
+++ JN2018.3/Assets/Game Jam Template/Scripts/Enemy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        print("hit");
+        print(collision.relativeVelocity.magnitude);
+        if (collision.relativeVelocity.magnitude > 1)
+        {
+            print("SMACK");
+            rb.AddRelativeForce(Vector3.forward * 500.0f);
+        }
+    }
+}
