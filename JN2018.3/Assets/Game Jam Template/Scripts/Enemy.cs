55914d3f32bc332f006287d705fce1565aee64d2
Justin Loranger-Ahluwalia <lorangera.justin@gmail.com>
2019-01-26 03:11:08 -0500

    Spawning enemies

--- JN2018.3/Assets/Game Jam Template/Scripts/Enemy.cs
+++ JN2018.3/Assets/Game Jam Template/Scripts/Enemy.cs
@@ -5,27 +5,76 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public Rigidbody rb;
+    public int timeToDeath = 3;
+    public int smackForce = 10;
+
+    public float speed = 1.0f;
+    public float escapeSpeed = 2.0f;
+    public int goldSteal = 5;
+    public bool hasGold = false;
+
+    public Transform target;
+    public Transform escapeTarget;
+
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        target = GameObject.FindWithTag("TargetZone").transform;
+        escapeTarget = GameObject.FindWithTag("EscapeZone").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!hasGold)
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+        }
+        else
+        {
+            float step = escapeSpeed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, escapeTarget.position, step);
+        }
     }
 
     void OnCollisionEnter(Collision collision)
     {
-        print("hit");
-        print(collision.relativeVelocity.magnitude);
-        if (collision.relativeVelocity.magnitude > 1)
+        //print("hit");
+        //print(collision.relativeVelocity.magnitude);
+
+        //Hit by player
+        if (collision.gameObject.tag == "PlayerCollider" && collision.relativeVelocity.magnitude > smackForce)
         {
             print("SMACK");
             rb.AddRelativeForce(Vector3.forward * 500.0f);
+            StartCoroutine(DeathTimer());
         }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Reaches gold
+        if (other.gameObject.tag == "TargetZone")
+        {
+            print("stealin yer gold!");
+            //Steal gold
+            hasGold = true;
+        }
+        //Escapes with gold
+        else if (other.gameObject.tag == "EscapeZone" && hasGold)
+        {
+            print("I escaped!");
+            //Escape
+            Destroy(gameObject);
+        }
+    }
+
+    IEnumerator DeathTimer()
+    {
+        yield return new WaitForSeconds(timeToDeath);
+        Destroy(gameObject);
     }
 }
---
+++ JN2018.3/Assets/Game Jam Template/Scripts/EnemySpawner.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    //Variables
+    public GameObject enemy;
+    public GameObject spawnCentre;
+    public float spawnTimeMin = 5.0f;
+    public float spawnTimeMax = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnEnemy", spawnTimeMin, spawnTimeMax);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnEnemy()
+    {
+        Vector3 position = new Vector3(spawnCentre.transform.position.x + Random.Range(-5.0f, 5.0f), spawnCentre.transform.position.y, spawnCentre.transform.position.z + Random.Range(-5.0f, 5.0f));
+        Instantiate(enemy, position, Quaternion.identity);
+        print("spawn at " + position);
+
+
+
+        //var newEnemy = GameObject.Instantiate(enemy);
+        //newEnemy.transform.position = Random.insideUnitSphere * 5;
+        //newEnemy.transform.position.y = 1;
+        //newEnemy.transform.position += spawnCentre.transform.position;
+        //newEnemy.transform.position.y = 1.0f;
+    }
+
+}
