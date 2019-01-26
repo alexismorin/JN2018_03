f0443bfd7e39e0023c16c4711d26b23a392f087a
Justin Loranger-Ahluwalia <lorangera.justin@gmail.com>
2019-01-26 18:43:21 -0500

    Added timer

--- JN2018.3/Assets/Game Jam Template/Scripts/GameController.cs
+++ JN2018.3/Assets/Game Jam Template/Scripts/GameController.cs
@@ -2,17 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
 
     public int startingLoot = 100;
     public int loot;
+    
+    public int enemiesKilled;
+
+    public Text timerText;
+    public float secondsCount;
+    public int minuteCount;
+    public int hourCount;
 
     // Start is called before the first frame update
     void Start()
     {
         loot = startingLoot;
+        enemiesKilled = 0;
+        secondsCount = 0.0f;
     }
 
     // Update is called once per frame
@@ -23,10 +33,30 @@ public class GameController : MonoBehaviour
             //Goes Back to Main Menu
             SceneManager.LoadScene(0);
         }
+
+        UpdateTimerUI();
     }
 
     public void LootChange(int change)
     {
         loot = loot + change;
+    }
+
+    public void UpdateTimerUI()
+    {
+        print("timer up");
+        //set timer UI
+        secondsCount += Time.deltaTime;
+        timerText.text = hourCount + "h:" + minuteCount + "m:" + (int)secondsCount + "s";
+        if (secondsCount >= 60)
+        {
+            minuteCount++;
+            secondsCount = 0;
+        }
+        else if (minuteCount >= 60)
+        {
+            hourCount++;
+            minuteCount = 0;
+        }
     }
 }
