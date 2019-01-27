174ddc4819c8a2146bf4a397fab7c08d0babda92
Alexis <alexis.morin.mail@gmail.com>
2019-01-26 21:58:25 -0500

    smackableLoad

---
+++ JN2018.3/Assets/Game Jam Template/Scripts/SmackableMenu_LoadLevel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SmackableMenu_LoadLevel : MonoBehaviour {
+
+    public AudioSource audioS;
+    public int sceneToLoad;
+
+    void OnCollisionEnter (Collision collision) {
+
+        if (collision.gameObject.tag == "PlayerCollider" && collision.relativeVelocity.magnitude > 0.001f) {
+            print ("loading");
+            audioS.Play ();
+            Invoke ("Load", 1f);
+        }
+    }
+
+    void Load () {
+        SceneManager.LoadScene (sceneToLoad, LoadSceneMode.Single);
+    }
+}
